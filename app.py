c1c0b7f30ad79c7720d606e8adf25df531533fb2
TriEightz <yujin_g1995@hotmail.com>
2024-03-24 15:07:58 +0800

    trying small model first

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import Flask,render_template,request
 # from audiocraft.data.audio import audio_write
 import os
 
-# model = musicgen.MusicGen.get_pretrained('facebook/musicgen-melody', device='cpu')
+model = MusicGen.get_pretrained("small", device='cpu')
 
 app = Flask(__name__)
 
@@ -20,16 +20,11 @@ def index():
 @app.route("/result_melody",methods=["GET","POST"])
 def result_palm():
     prompt_text = request.form.get("q") 
-    # model.set_generation_params(duration=1)
-    # res = model.generate( [ prompt_text ], progress=True)
-    # for idx, one_wav in enumerate(res):
-    #     audio_write('static/audio_file', one_wav.cpu(), model.sample_rate)
-    
-    from gradio_client import Client
-
-    client = Client("https://facebook-musicgen.hf.space/")
-    result = client.predict(prompt_text, None, fn_index=0)
-    return(render_template("result_melody.html",r=result[0]))
+    model.set_generation_params(duration=5)
+    res = model.generate( [ prompt_text ], progress=True)
+    for idx, one_wav in enumerate(res):
+        audio_write('static/audio_file', one_wav.cpu(), model.sample_rate)
+    return(render_template("result_melody.html",r='static/audio_file.wav"))
 
 @app.route("/end",methods=["GET","POST"])
 def end():
